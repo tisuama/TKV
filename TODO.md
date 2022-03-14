7de2717b3040414704e0c63e62649fe29542b11a
tisuama <1754121135@qq.com>
2022-03-15 02:17:38 +0800

    [Update] add meta server main

---
+++ TODO.md
@@ -0,0 +1,5 @@
+## TVK TODO
+### 1. Simple build meta & store first
+- meta server
+- store server
+
---
+++ src/meta/main.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <fstream>
+#include "gflags/gflags.h"
+
+
+namespace TKV {
+
+int main(int argc, char** arg[]) {
+
+	// read flags file
+}
+
+
+
+
+}
